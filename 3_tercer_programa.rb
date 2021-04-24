47247c4b07bf4eaefda0516150353904ff7b9c6e
Alberto González <gonzalezlafuentealberto@gmail.com>
2021-04-24 09:53:03 +0200

    [Ruby] : Renamed files

---
+++ 3_tercer_programa.rb
@@ -0,0 +1,14 @@
+# Comparaciones# Comparamos el string con el otro string e imprimimos el resultado por pantallaputs "Hola Mundo".eql?("Hola Mundo")# Comparamos dos variables, el resultado lo guardamos en otra variable
+# He imprimimos el resultado de la comparacion
+a = 2
+b = 3
+c = a.eql?(b)
+puts c
+
+# Pasar varios argumentos a un metodo
+# Agregamos en el 3 lugar del string la letra d
+puts "abc".insert(3, 'd')
+
+# Alias: Los alias son maneras distintas de nombrar al mismo metodoputs 
+[1,2,3,4,5].size
+puts [1,2,3,4,5].length
