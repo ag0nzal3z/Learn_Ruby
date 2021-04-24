f699a45cd9511db1369aac274ee6cb85701ddfb6
Alberto González <gonzalezlafuentealberto@gmail.com>
2021-04-24 16:48:01 +0200

    [Ruby] : Sistemas Numericos

---
+++ 5_sistemas_numericos.rb
@@ -0,0 +1,30 @@
+# Solo los nuemeros enteros se pueden representar en otro sistema numerico
+
+# Representar un numero en sistema Decimal
+a = 5
+b = 8
+c = 126
+puts a, b, c
+# Representar numeros en sistema Binario
+a = 0b101
+b = 0b1000
+c = 0b1111110
+puts a, b, c
+
+# Representar numeros en sistema Octal
+a = 0o5
+b = 0o10
+c = 0o176
+puts a, b, c
+
+# Otra manera de representar numeros octales
+a = 05
+b = 010
+c = 0176
+puts a, b, c
+
+# Representar numeros en sistema Hexadecimal
+a = 0x5
+b = 0x8
+c = 0x7e
+puts a, b, c
