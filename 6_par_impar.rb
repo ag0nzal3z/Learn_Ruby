e3260783a0aa89d6183634cec11a7eb053a78354
Alberto González <gonzalezlafuentealberto@gmail.com>
2021-04-24 16:55:37 +0200

    [Ruby] : El numero es Par o Impar

---
+++ 6_par_impar.rb
@@ -0,0 +1,14 @@
+# Numeros pares o impares
+a = 1
+b = 2
+
+# Preguntamos si el numero es "impar" .odd o si es "par" .even
+puts a.odd?
+puts a.even?
+
+puts b.odd?
+puts b.even?
+
+# Es el numero 23, impar? es par?
+puts 23.odd?
+puts 23.even?
