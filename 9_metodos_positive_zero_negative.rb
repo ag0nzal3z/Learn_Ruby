5e5b22c8a89f7b22d9ebb8b844795e62a8560e57
Alberto González <gonzalezlafuentealberto@gmail.com>
2021-04-25 11:10:12 +0200

    [Ruby] : Metodo positive, negative, zero y abs

---
+++ 9_metodos_positive_zero_negative.rb
@@ -0,0 +1,9 @@
+# Los metodos positive, zero, negative devuelven un valor booleano
+unidades = -2
+
+puts unidades.positive?
+puts unidades.zero?
+puts unidades.negative?
+
+# El metodo abs nos devolvera el valor absoluto, el valor numerico son considerar el signo
+puts unidades.abs
